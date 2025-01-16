d9826a86cfc16c220ac8013e732aeb1a2e78147b
Aline <alinedanieleds@gmail.com>
2025-01-16 08:12:48 +0000

    Inclusion of POST test

--- src/test/java/com/training/tasks/api/steps/TasksSteps.java
+++ src/test/java/com/training/tasks/api/steps/TasksSteps.java
@@ -1,15 +1,23 @@
 package com.training.tasks.api.steps;
 
 import io.cucumber.java.en.*;
+import io.restassured.RestAssured;
 import io.restassured.response.Response;
 
 import static io.restassured.RestAssured.*;
 import static org.assertj.core.api.Assertions.*;
+import static org.hamcrest.Matchers.*;
+
+import org.springframework.beans.factory.annotation.Value;
 
 public class TasksSteps {
 
     private Response response;
+    private String payload;
 
+     @Value("${service.url}")
+    private String apiUrl;
+    
     @When("the client calls GET Tasks")
     public void the_client_calls_GET_Tasks() {
         response = when().get("/tasks");
@@ -18,5 +26,35 @@ public class TasksSteps {
     @Then("the api returns with status code {int}")
     public void the_api_returns_status_code_of(int statusCode) {
         assertThat(response.getStatusCode()).isEqualTo(statusCode);
+    }
+
+    @Given("I have a valid payload to create an appointment")
+    public void validAppointmentPayload(String body) {
+        payload = body;
+    }
+
+    @When("I send a POST request to {string}")
+    public void iSendAPostRequestTo(String endpoint) {
+        RestAssured.baseURI = apiUrl; 
+        response = 
+            given()
+                .header("Content-Type", "application/json")
+                .body(payload)
+            .when()
+                .post(endpoint);
+    }
+
+    @Then("the response status code should be {int}")
+    public void theResponseStatusCodeShouldBe(int statusCode) {
+        response.then().statusCode(statusCode);
+    }
+
+     @Then("the response should contain the appointment details")
+    public void theResponseShouldContainTheAppointmentDetails() {
+        response.then()
+            .body("title", equalTo("appointment"))
+            .body("description", equalTo("just a new appointment"))
+            .body("completionDate", equalTo("2025-01-18T14:00:00"))
+            .body("completed", equalTo(false));
     }
 }
--- src/test/resources/features/Tasks.feature
+++ src/test/resources/features/Tasks.feature
@@ -3,3 +3,11 @@ Feature: This is a sample features test
   Scenario: client makes call to GET /tasks
     When the client calls GET Tasks
     Then the api returns with status code 200
+
+  Scenario: Successfully create a new appointment
+    
+    Given I have a valid payload to create an appointment
+    When I send a POST request to "/appointments"
+    Then the response status code should be 201
+    And the response should contain the appointment details
+
